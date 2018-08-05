92c86779825e1536d9345d93b9e96c347aa30984
Guest User <Guest@epicodus-94.local>
2018-08-04 20:18:32 -0700

    Have imported all Packets/moduels, Still need to create actual project/test

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//import './styles.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//import $ from 'jquery';
+//import { SpaceAge } from './Space-Age';
+
+//
+// $(document).ready(function() {
+//   $('#Space-Age-form').submit(function(event) {
+//     event.preventDefault();
+//     var goal = $('#goal').val();
+//     var output = SpaceAge(goal);
+//     output.forEach(function(element) {
+//       $('#solution').append("<li>" + element + "</li>");
+//     });
+//   });
+// });
