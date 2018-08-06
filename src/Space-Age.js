50a5901f5249026e34ca0ac6d2ff9a4580b58ad1
Brian <brianpalowski00@gmail.com>
2018-08-06 11:35:22 -0700

    Test for Spec should test that user age and test checking second age to age pass

--- src/Space-Age.js
+++ src/Space-Age.js
@@ -1,43 +1,13 @@
-export class GalacticAgeCalculator {
+export class SpaceAge{
   constructor(userAge, userLife){
     this.userAge = userAge;
     this.userLife = userLife;
-  }
 
-  checkAgeInSeconds() {
-    let userDate = new Date(this.userAge)
+  }
+  checkAgeInSeconds(){
+    let userDate = new Date (this.userAge);
     let todayDate = new Date();
     return Math.floor((todayDate.getTime() - userDate.getTime() / 1000));
   }
-
-  checkAgeOnEarth(){
-    let EarthAge = Math.floor(this.checkAgeInSeconds() / 31536000);
-    return EarthAge;
-  }
-
-  checkAgeOnMercury(){
-    let MercuryAge = Math.floor(this.checkAgeInSeconds() / 31536000 / .24);
-    return MercuryAge;
-  }
-
-  checkAgeOnVenus(){
-    let VenusAge = Math.floor(this.checkAgeInSeconds() / 31536000 / .62);
-    return VenusAge;
-  }
-
-  checkAgeOnMars(){
-    let MarsAge = Math.floor(this.checkAgeInSeconds() / 31536000 / 1.88);
-    return MarsAge;
-  }
-
-  checkAgeOnJupiter(){
-    let JupiterAge = Math.floor(this.checkAgeInSeconds() / 31536000 / 11.86);
-    return JupiterAge;
-
-  }
-
-  lifeExpectancy(){
-    let userLifeExpectancy = 0;
-  }
-
+  check
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
-import { GalacticAgeCalculator } from './Space-Age.js';
+import { SpaceAge } from './Space-Age.js';
 
 $(document).ready(function() {
   $('#').submit(function(event) {
     event.preventDefault();
     //var row = $('#').val();
 
-    var output = new GalacticAgeCalculator();
-    output.checkHaiku();
+    var output = new SpaceAge();
 
     $('ul#').append("<h1>" + "<p>" + output + "</p>" + "</h1>");
 
